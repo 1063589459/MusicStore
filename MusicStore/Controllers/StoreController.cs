9a885c4112b1b27e541b726f3e7d5095813d1986
潘水龙 <1063589459@qq.com>
2017-10-18 18:48:37 +0800

    add database

--- MusicStore/Controllers/StoreController.cs
+++ MusicStore/Controllers/StoreController.cs
@@ -11,26 +11,34 @@ namespace MusicStore.Controllers
         //
         // GET: /Store/
 
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
         public ActionResult Index()
         {
-            var genres = new List<Genre>
-            {
-                new Genre { Name = "Disco" },
-                new Genre { Name = "Jazz" },
-                new Genre { Name = "Rock" }
-            };
-            return View(genres);
+            //var genres = new List<Genre>
+            //{
+            //    new Genre { Name = "Disco" },
+            //    new Genre { Name = "Jazz" },
+            //    new Genre { Name = "Rock" }
+            //};
+            //return View(genres);
+            var genres = storeDB.Genres.ToList();
+            return this.View(genres);
         }
 
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            //var genreModel = new Genre { Name = genre };
+            //return View(genreModel);
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
             return View(genreModel);
         }
 
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            //var album = new Album { Title = "Album " + id };
+            //return View(album);
+            var album = storeDB.Albums.Find(id);
             return View(album);
         }
 
